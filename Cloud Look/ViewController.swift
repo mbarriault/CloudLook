e756b8bc282a33550e1a6ccebde079d13d9b33b6
Michael Barriault <mikebarriault@me.com>
2014-12-14 20:37:14 -0330

    Initial Commit

---
+++ Cloud Look/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cloud Look
+//
+//  Created by Mike Barriault on 2014-12-14.
+//  Copyright (c) 2014 Mike Barriault. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
